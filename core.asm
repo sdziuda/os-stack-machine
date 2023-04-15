7ca2cd3ca032d2f6c7881c51846d0d236ab9f022
sdziuda <szymondziuda95@gmail.com>
2023-04-15 17:53:43 +0200

    Removed redundant comments
    Also changed imul to mul

--- core.asm
+++ core.asm
@@ -21,94 +21,94 @@ core:
 
 .check_plus:
         cmp     al, '+'                 ; sprawdzamy czy znak to '+'
-        jne     .check_star             ; jeśli nie, to sprawdzamy czy to '*'
+        jne     .check_star             ; jeśli nie, to przechodzimy do sprawdzania czy to '*'
         pop     rax                     ; jeśli tak, to pobieramy dwie wartości ze stosu
         pop     rdx
         add     rax, rdx                ; dodajemy je
         push    rax                     ; i wynik wrzucamy na stos
-        jmp     .increment              ; przechodzimy dalej
+        jmp     .increment              ; pozostałych warunków nie musimy sprawdzać, przechodzimy dalej
 
 .check_star:
         cmp     al, '*'                 ; sprawdzamy czy znak to '*'
-        jne     .check_minus            ; jeśli nie, to sprawdzamy czy to '-'
+        jne     .check_minus            ; jeśli nie, to przechodzimy do sprawdzania czy to '-'
         pop     rax                     ; jeśli tak, to pobieramy dwie wartości ze stosu
         pop     rdx
-        imul    rax, rdx                ; mnożymy je
+        mul     rdx                     ; mnożymy je
         push    rax                     ; i wynik wrzucamy na stos
-        jmp     .increment              ; przechodzimy dalej
+        jmp     .increment
 
 .check_minus:
         cmp     al, '-'                 ; sprawdzamy czy znak to '-'
-        jne     .check_n                ; jeśli nie, to sprawdzamy czy to 'n'
+        jne     .check_n                ; jeśli nie, to przechodzimy do sprawdzania czy to 'n'
         pop     rax                     ; jeśli tak, to pobieramy wartość ze stosu
         neg     rax                     ; negujemy ją arytmetycznie
         push    rax                     ; i wynik wrzucamy na stos
-        jmp     .increment              ; przechodzimy dalej
+        jmp     .increment
 
 .check_n:
         cmp     al, 'n'                 ; sprawdzamy czy znak to 'n'
-        jne     .check_B                ; jeśli nie, to sprawdzamy czy to 'B'
+        jne     .check_B                ; jeśli nie, to przechodzimy do sprawdzania czy to 'B'
         push    rdi                     ; jeśli tak, to wrzucamy na stos wartość n (trzymaną w rdi)
-        jmp     .increment              ; przechodzimy dalej
+        jmp     .increment
 
 .check_B:
         cmp     al, 'B'                 ; sprawdzamy czy znak to 'B'
-        jne     .check_C                ; jeśli nie, to sprawdzamy czy to 'C'
-        jmp     .increment              ; przechodzimy dalej
+        jne     .check_C                ; jeśli nie, to przechodzimy do sprawdzania czy to 'C'
+        jmp     .increment
 
 .check_C:
         cmp     al, 'C'                 ; sprawdzamy czy znak to 'C'
-        jne     .check_D                ; jeśli nie, to sprawdzamy czy to 'D'
+        jne     .check_D                ; jeśli nie, to przechodzimy do sprawdzania czy to 'D'
         pop     rax                     ; jeśli tak, to pobieramy wartość ze stosu i nie wrzucamy z powrotem
-        jmp     .increment              ; przechodzimy dalej
+        jmp     .increment
 
 .check_D:
         cmp     al, 'D'                 ; sprawdzamy czy znak to 'D'
-        jne     .check_E                ; jeśli nie, to sprawdzamy czy to 'E'
+        jne     .check_E                ; jeśli nie, to przechodzimy do sprawdzania czy to 'E'
         pop     rax                     ; jeśli tak, to pobieramy wartość ze stosu
         push    rax                     ; wrzucamy ją na stos
         push    rax                     ; i jeszcze raz
-        jmp     .increment              ; przechodzimy dalej
+        jmp     .increment
 
 .check_E:
         cmp     al, 'E'                 ; sprawdzamy czy znak to 'E'
-        jne     .check_G                ; jeśli nie, to sprawdzamy czy to 'G'
+        jne     .check_G                ; jeśli nie, to przechodzimy do sprawdzania czy to 'G'
         pop     rax                     ; jeśli tak, to pobieramy dwie wartości ze stosu
         pop     rdx
         push    rax                     ; wrzucamy je na stos w odwrotnej kolejności
         push    rdx
-        jmp     .increment              ; przechodzimy dalej
+        jmp     .increment
 
 .check_G:
         cmp     al, 'G'                 ; sprawdzamy czy znak to 'G'
-        jne     .check_P                ; jeśli nie, to sprawdzamy czy to 'P'
+        jne     .check_P                ; jeśli nie, to przechodzimy do sprawdzania czy to 'P'
         call    get_value               ; jeśli tak, to wołamy funkcję get_value (parametr n jest w rdi)
         push    rax                     ; i jej wynik wrzucamy na stos
-        jmp     .increment              ; przechodzimy dalej
+        jmp     .increment
 
 .check_P:
         cmp     al, 'P'                 ; sprawdzamy czy znak to 'P'
-        jne     .check_S                ; jeśli nie, to sprawdzamy czy to 'S'
+        jne     .check_S                ; jeśli nie, to przechodzimy do sprawdzania czy to 'S'
         mov     rdx, rsi                ; wpp. kopiujemy adres tablicy znaków (rsi) do rdx
         pop     rsi                     ; pobieramy wartość ze stosu do rsi
         push    rdx                     ; wrzucamy adres tablicy znaków na stos (aby został zachowany przy call)
         call    put_value               ; wołamy funkcję put_value (parametr n jest w rdi a v w rsi)
         pop     rsi                     ; przywracamy wartość rsi
-        jmp     .increment              ; przechodzimy dalej
+        jmp     .increment
 
 .check_S:
         cmp     al, 'S'                 ; sprawdzamy czy znak to 'S'
-        jne     .check_number           ; jeśli nie, to sprawdzamy czy to cyfra
-        jmp     .increment              ; przechodzimy dalej
+        jne     .number                 ; jeśli nie, to zakładamy, że znak to cyfra 0-9
+        jmp     .increment
 
-.check_number:                          ; zakładamy teraz, że znak to cyfra 0-9
+.number:                                ; zakładamy teraz, że znak to cyfra 0-9
         sub     al, '0'                 ; odejmujemy od znaku '0' aby uzyskać cyfrę
         movzx   eax, al                 ; rozszerzamy cyfrę do 64 bitów (aby móc ją wrzucić na stos)
         push    rax                     ; wrzucamy cyfrę na stos
 
 .increment:
-        inc     rcx                     ; zwiększamy indeks
-        jmp     .read_loop              ; powtarzamy pętlę
+        inc     rcx
+        jmp     .read_loop
 
 .end:
         pop     rax                     ; wynik jest na szczycie stosu
